91b37aaa96c477f141d359efa0a1021ef7c96a1f
TeeOmyko <amanwachiteresa@gmail.com>
2023-04-26 22:08:47 +0000

    cocktail sort list

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,80 +1,85 @@
-nclude "sort.h"
+#include "sort.h"
 
 /**
- * swap - function that swaps nodes in a doubly linked list of integers
- * @a: first node to be swapped
- * @b: second node to be swapped
+ * swap - function that swaps nodes in a linked
+ * list of integers
+ * @current_node: current position to swap
+ * @next_node: next position to be swapped
  * @list: doubly linked list of integers
  * Return: void
  */
-void swap(listint_t *a, listint_t *b, listint_t **list)
+void swap(listint_t *current_node, listint_t *next_node, listint_t **list)
 {
-	/* Update the next and previous pointers of the nodes being swapped */
-	a->next = b->next;
-	b->prev = a->prev;
-	if (a->next)
-		a->next->prev = a;
-	if (b->prev)
-		b->prev->next = b;
-
-	/* Update the next and previous pointers of the nodes surrounding the swapped nodes */
-	a->prev = b;
-	b->next = a;
-	if (a == *list)
-		*list = b;
+	/*mandamos el 99, mandamos el 71 y mandamos la cabeza 19*/
+	/*el next del 99 apunta al 13*/
+	current_node->next = next_node->next;
+	/*el 13 existe ?*/
+	if (current_node->next)
+		/*el 13 prev apunta al 99*/
+		current_node->next->prev = current_node;
+	/*el 71 next apunta al 99*/
+	next_node->next = current_node;
+	/*el 71 prev apunta al 48*/
+	next_node->prev = current_node->prev;
+	/*el 48 existe ?*/
+	if (next_node->prev)
+		/*el 48 next apunta al 71*/
+		next_node->prev->next = next_node;
+	else
+		*list = next_node;
+	/*el 99 prev apunta al 71*/
+	current_node->prev = next_node;
 
 	print_list(*list);
 }
 
 /**
- * cocktail_sort_list - function that sorts a doubly linked list of integers
- * in ascending order using the Cocktail shaker sort algorithm
+ * cocktail_sort_list - function that sorts a doubly linked
+ * list of integers in ascending order using the Cocktail
+ * shaker sort algorithm
  * @list: doubly linked list of integers
  * Return: void
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *left, *right;
+	listint_t *front_runner, *back_runner;
 	int swapped = 1;
 
 	if (list == NULL || *list == NULL)
 		return;
-
-	left = *list;
-	right = NULL;
-
+	front_runner = (*list);
+	back_runner = NULL;
 	while (swapped == 1)
 	{
-		/* Here we are going forward */
+		/*Here we are going forward*/
 		swapped = 0;
-		while (left->next != right)
+		/*19, 48, 99, 71, 13, 52, 96, 73, 86, 7*/
+		while (front_runner->next && front_runner != back_runner)
 		{
-			if (left->n > left->next->n)
+			/*99 es mayor que 71 ?*/
+			if (front_runner->n > front_runner->next->n)
 			{
-				swapped = 1;
-				swap(left, left->next, list);
+				swapped = 1;/*mandamos el 99, el 71 y la cabeza 19*/
+				swap(front_runner, front_runner->next, list);
 			}
 			else
-				left = left->next;
+				front_runner = front_runner->next;
 		}
-
-		right = left;
+		back_runner = front_runner;
 		if (swapped == 0)
 			break;
-
-		/* Here we are going backward */
+		/*Here we are going backward*/
 		swapped = 0;
-		while (right->prev != NULL)
+		while (front_runner->prev && front_runner != *list)
 		{
-			if (right->n < right->prev->n)
+			if (front_runner->n < front_runner->prev->n)
 			{
 				swapped = 1;
-				swap(right->prev, right, list);
+				swap(front_runner->prev, front_runner, list);
 			}
 			else
-				right = right->prev;
+				front_runner = front_runner->prev;
 		}
-
-		left = right;
+		*list = front_runner;
 	}
 }
